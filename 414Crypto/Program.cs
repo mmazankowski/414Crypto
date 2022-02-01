801d41bd52898b926b1f928b0ce6e9ed6cce4041
Matthew Mazankowski <m.g.mazankowski@gmail.com>
2022-01-31 22:26:22 -0700

    Initial commit

---
+++ 414Crypto/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace _414Crypto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
